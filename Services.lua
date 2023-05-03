2cccf14cc41fc2917f7bf64f6935aa03ab072151
Alex <84569950+Miragex1337@users.noreply.github.com>
2023-05-03 15:51:32 +0200

    Update Services.lua

--- Services.lua
+++ Services.lua
@@ -1,4 +1,4 @@
-local Services = setmetatable({}, {
+return setmetatable({}, {
     __index = function(...)
         assert(select(2,...) and game.GetService(game, select(2,...)), string.format("Invalid service name: \"%s\"", select(2,...)))
         return game.GetService(game, select(2,...))
